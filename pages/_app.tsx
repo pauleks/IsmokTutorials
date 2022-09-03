5e55a805d259498c0368bcb9139ebf3bbc6459ee
Ghostwolf <git@paulekas.eu>
2022-09-03 21:56:02 +0300

    Add loading indicator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider, useTheme } from "next-themes";
 import { MDXProvider } from '@mdx-js/react'
 import Head from "next/head";
 import Image from "next/image";
@@ -8,6 +8,7 @@ import Image from "next/image";
 import Navbar from "../components/NavigationBar";
 import Footer from "../components/Footer";
 import PointsPreview from "../components/PointsPreview";
+import NextNProgress from "nextjs-progressbar";
 
 const ResponsiveImage = (props: any) => <Image alt={props.alt} layout="responsive" {...props} />
 
@@ -16,12 +17,14 @@ const components = {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { theme } = useTheme();
+
   return (
     <MDXProvider components={components}>
       <ThemeProvider>
-        <Head>
-          <link rel="icon" type="image/x-icon" href="favicon.png" />
-        </Head>
+        <NextNProgress
+          color={theme === "dark" ? "#f0f0f0" : "#141414"}
+        />
         <PointsPreview />
         <Navbar />
         <Component {...pageProps} />

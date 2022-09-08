fb3c4b36d55601ca8c7fa8c8b0ab6699f295f678
Paulius Gečas <git@paulekas.eu>
2022-09-08 13:02:49 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,6 @@ const Home = ({ CategoriesData }: { CategoriesData: CategoryInterface[] }) => {
       <div className={styles.introBanner}>
         <p>Sveiki atvykę į</p>
         <h1>IšmOK</h1>
-        <p>Lengvai ir paprastai paaiškintos programavimo pamokos</p>
       </div>
       {categoriesList}
     </>

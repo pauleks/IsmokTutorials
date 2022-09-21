6ca9d5638de58f09cc75ab8dd2f6be7e24e3e827
Ghostwolf <git@paulekas.eu>
2022-09-21 20:50:31 +0300

    Fix build error

--- pages/api/v1/getAllData.ts
+++ pages/api/v1/getAllData.ts
@@ -1,9 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import CategoriesData from "../../../data/Categories.json";
+//import CategoriesData from "../../../data/Categories.json";
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  res.json(CategoriesData);
+  res.json({ hello: "world" });
 };
 
 export default handler;
